f7f8b53edc5b3b4893994c90b80163e3291f3596
Evgenken <politeh.evgen@gmail.com>
2022-01-22 13:22:41 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <Petrovich />
+  </div>
+</template>
+
+<script>
+import Petrovich from "@/components/Petrovich/petrovich";
+export default {
+  name: 'App',
+  components: {Petrovich}
+}
+</script>
+
