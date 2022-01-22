b85a036bd5d51dcaa07a43405a2de042a56693d6
Evgenken <politeh.evgen@gmail.com>
2022-01-22 13:33:03 +0300

    pictures and files

---
+++ src/components/Petrovich/petovich-card.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="card">
+    <div class="card__name">{{text.name}}</div>
+    <div class="card__titul">{{text.titul}}</div>
+    <div class="card__prof">{{ text.prof }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PetrovichCard',
+
+  props: {
+    text: {
+      type: Array,
+      default: ()=> []
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.card {
+  border: 1px solid white;
+  padding: 10px;
+  width: 100%;
+
+  &__name {
+    color: white;
+    text-align: center;
+  }
+
+  &__titul {
+    color: white;
+    text-align: right;
+  }
+
+  &__prof {
+    color: white;
+    text-align: left;
+  }
+}
+
+</style>
---
+++ src/components/Petrovich/petrovich.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="qwer">
+    <div class="qwer__asdf">asdfasdf</div>
+    <div  class="qwer" v-for="(item, key) in qwer" :key="key">
+      <petrovich-card :text="item" />
+    </div>
+  </div>
+</template>
+
+<script>
+import PetrovichCard from '@/components/Petrovich/petovich-card.vue';
+export default {
+  name: 'Petrovich',
+  components: {PetrovichCard},
+
+  data() {
+    return {
+      qwer: [
+        {name: 'Evgen', titul: 'huevii', prof: 'electrik' },
+        {name: 'Arsd', titul: 'Luch', prof: 'Code' },
+        {name: 'Mih', titul: 'middle', prof: 'Test' }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.qwer {
+  width: 100%;
+  background: red;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 200px;
+  flex-wrap: wrap;
+
+  &__asdf {
+    background: green;
+    width: 50%;
+    height: 100px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+
+</style>
