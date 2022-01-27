fbe43ee4b951a1546321bff182314a92ee532943
Evgenken <politeh.evgen@gmail.com>
2022-01-27 22:15:29 +0300

    pictures and files

--- src/components/Petrovich/petrovich.vue
+++ src/components/Petrovich/petrovich.vue
@@ -1,18 +1,27 @@
 <template>
+  <div>
   <div class="intro">
-    <div class="intro__background"><img src="@/assets/img/background.png"></div>
     <div class="logo">
       <div class="logo__item-1"><img src="@/assets/img/logo.png"> </div>
       <div class="logo__item-2"><img src="@/assets/img/SVGID.png"></div>
       </div>
           <div class="intro__text">лайфхаки и сервисы, которые пригодятся всем</div>
-          <div class="circle-1"><img src="@/assets/img/Ellipse 8.png"></div>
     <div class="img-slogan">
+      <div class="circle-1"><img src="@/assets/img/Ellipse 8.png"></div>
       <div class="img-slogan__instrument-1"><img src="@/assets/img/roulette.png"></div>
       <div class="img-slogan__letters"><img src="@/assets/img/letters.png" alt=""></div>
       <div class="img-slogan__instrument-2"><img src="@/assets/img/bucket.png"></div>
   </div>
     <div class="stair"><img src="@/assets/img/stair.png"></div>
+  </div>
+  <div>
+    <div class="background">
+      <img src="@/assets/img/background3.png" class="background_item-1">
+      <img src="@/assets/img/Rectangle 1239.png" class="background__item-2">
+      <img src="@/assets/img/Ellipse 15.png" class="background__item-3">
+    </div>
+
+  </div>
   </div>
 </template>
 
@@ -21,12 +30,20 @@
 </script>
 
 <style scoped lang="scss">
-
+*{
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
 /* Intro */
 .intro{
-  height: 768px;
+  height: 690px;
   width: 100%;
   overflow: hidden;
+  display: flex;
+  flex-direction:column;
+  background: #F8CB46;
+  flex: 1 0 auto;
 }
 .intro__text {
   font-family: "Graphik Cy", sans-serif;
@@ -37,9 +54,7 @@
   padding-top: 91px;
   position: relative;
 }
-.intro__background{
-  position: absolute;
-}
+
 .logo {
   display: flex;
 justify-content: space-between;
@@ -56,12 +71,18 @@ justify-content: space-between;
 }
 
 .stair{
-  position: relative;
-  text-align: center;
-  padding-right: 200px;
-  z-index: 2;
+  z-index: 3;
   animation-duration: 3s;
   animation-name: stair;
+  position: absolute;
+  bottom: 145px;
+  left: 500px;
+}
+.background{
+
+ &__item-1{
+width-max:100% ;
+ }
 }
 /*img*/
 
@@ -70,7 +91,6 @@ justify-content: space-between;
   display: flex;
   justify-content: center;
   padding-top: 20px;
-  box-sizing: border-box;
   &__letters{
     z-index: 3;
   }
@@ -113,15 +133,13 @@ width: 18.3%;
     height: 100%;
   }
   to{
-    margin-top: 500px;
+    margin-top: -800px;
     height: 12.1%;
   }
 }
 /* circle */
 
 .circle-1{
-  position: absolute;
-  justify-content: center;
-  padding-left: 250px;
+
 }
 </style>
